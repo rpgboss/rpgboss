7e0e2a159840ea791a351ce80c5a6edfa6a22df8
Tommy C. Li <tommycli@keepword.com>
2012-10-18 00:16:11 -0700

    Edit main script

--- src/main/resources/defaultrc/script/main.js
+++ src/main/resources/defaultrc/script/main.js
@@ -2,7 +2,7 @@ function main() {
   game.setTransition(0, 1, 600);
   game.showPicture(0, "LordSpirit.jpg", 0, 0, 640, 480);
   var winW = 200;
-  choiceIdx = game.choiceWindow(
+  choiceIdx = game.showChoices(
     ["New Game", "Load Game", "Quit"],
     320-winW/2, 280, winW, 130,
     game.CENTER())
