471be1cd18321d12fe7da65a64f291cc7facab1c
Tommy C. Li <tommycli@keepword.com>
2013-10-28 23:05:09 -0700

    consistent name

---
+++ common/src/main/scala/rpgboss/model/resource/Battler.scala
@@ -0,0 +1,20 @@
+package rpgboss.model.resource
+
+import rpgboss.model._
+
+case class BattlerMetadata()
+
+case class Battler(proj: Project, name: String,
+                   metadata: BattlerMetadata)
+  extends ImageResource[Battler, BattlerMetadata] {
+  def meta = Battler
+
+}
+
+object Battler extends MetaResource[Battler, BattlerMetadata] {
+  def rcType = "Battler"
+  def keyExts = Array("png", "jpg", "jpeg")
+
+  def defaultInstance(proj: Project, name: String) =
+    Battler(proj, name, BattlerMetadata())
+}
