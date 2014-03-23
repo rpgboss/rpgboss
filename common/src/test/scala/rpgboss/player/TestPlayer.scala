420bbd23ceff4e4aa4d3a13af132bc4bb2091c50
Tommy C. Li <tommycli@keepword.com>
2014-03-23 11:06:12 -0700

    update tests to new libgdx

--- common/src/test/scala/rpgboss/player/TestPlayer.scala
+++ common/src/test/scala/rpgboss/player/TestPlayer.scala
@@ -12,7 +12,7 @@ import org.lwjgl.opengl.Display
  */
 class TestGameContainer extends ApplicationListener {
   var childGame: Option[MyGame] = None
-  
+
   // Can be called on any thread, since it will post to the Gdx thread.
   def replace(game: MyGame) = {
     Gdx.app.postRunnable(new Runnable() {
@@ -24,25 +24,25 @@ class TestGameContainer extends ApplicationListener {
       }
     })
   }
-  
+
   def create() = {}
-  
+
   def dispose() = {
     childGame.map(_.dispose())
   }
-  
+
   def pause() = {
     childGame.map(_.pause())
   }
-  
+
   def render() = {
     childGame.map(_.render())
   }
-  
+
   def resize(x: Int, y: Int) = {
     childGame.map(_.resize(x, y))
   }
-  
+
   def resume() = {
     childGame.map(_.resume())
   }
@@ -51,19 +51,18 @@ class TestGameContainer extends ApplicationListener {
 object TestPlayer {
   val container = new TestGameContainer
   var app: LwjglApplication = null
-  
+
   def launch(game: MyGame) = {
     val conf = new LwjglApplicationConfiguration();
     conf.title = game.project.data.title;
     conf.width = 32 * 20;
     conf.height = 32 * 15;
-    conf.useGL20 = true;
     conf.forceExit = false;
 
     if (app == null) {
       app = new LwjglApplication(container, conf)
     }
-    
+
     container.replace(game)
   }
 }
