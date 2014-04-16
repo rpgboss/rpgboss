5340692b58f3d5ecf0fb67f50470d9fd11acf8cf
Tommy C. Li <tommycli@keepword.com>
2014-04-15 22:13:41 -0700

    damage calculation works now

--- common/src/main/scala/rpgboss/model/Skill.scala
+++ common/src/main/scala/rpgboss/model/Skill.scala
@@ -1,5 +1,6 @@
 package rpgboss.model
 
+import com.typesafe.scalalogging.slf4j.Logging
 import org.mozilla.javascript.{ Context, ScriptableObject, Scriptable }
 import rpgboss.model.battle.BattleStatus
 
@@ -8,53 +9,60 @@ object DamageType extends RpgEnum {
   def default = Physical
 }
 
-class JSBattleEntity(hp: Int, atk: Int) extends ScriptableObject {
-  /**
-   * No-arg constructor used to construct the Javascript prototype object.
-   */
-  def this() = { this(0, 0) }
-  
+class JSBattleEntity(status: BattleStatus) extends ScriptableObject {
   def getClassName = classOf[JSBattleEntity].getName()
-
-  def jsGet_hp() = hp
-  def jsGet_atk() = atk
-}
-
-object JSBattleEntity {
-  def apply(x: BattleStatus) = new JSBattleEntity(
-      x.hp,
-      x.stats.atk)
+  
+  override def get(name: String, start: Scriptable): java.lang.Integer = {
+    name match {
+      case "hp" => status.hp
+      case "mp" => status.mp
+      case "mhp" => status.stats.mhp
+      case "mmp" => status.stats.mmp
+      case "atk" => status.stats.atk
+      case "spd" => status.stats.spd
+      case "mag" => status.stats.mag
+      case "arm" => status.stats.arm
+      case "mre" => status.stats.mre
+      case _ => -1
+    }
+  }
 }
 
 case class Damage(
   var typeId: Int = DamageType.Physical.id,
   var elementId: Int = 0,
-  var formula: String = "") {
+  var formula: String = "") extends Logging {
   
-  def getBaseDamage(source: BattleStatus, target: BattleStatus) = {
+  def getBaseDamage(source: BattleStatus, target: BattleStatus): Int = {
     val jsContext = Context.enter()
     val jsScope = jsContext.initStandardObjects()
-    ScriptableObject.defineClass(jsScope, classOf[JSBattleEntity])
     
     ScriptableObject.putProperty(
-      jsScope, "a", Context.javaToJS(source, jsScope))
+      jsScope, "a", Context.javaToJS(new JSBattleEntity(source), jsScope))
     ScriptableObject.putProperty(
-      jsScope, "b", Context.javaToJS(target, jsScope))
+      jsScope, "b", Context.javaToJS(new JSBattleEntity(target), jsScope))
+
+    try {
+      val jsResult = jsContext.evaluateString(
+        jsScope, 
+        formula, 
+        "DamageFormula: %s".format(formula),
+        1, 
+        null)
+          
+      val resultAsInt = Context.toNumber(jsResult).round.toInt
+    
+      Context.exit()
       
-    val jsResult = jsContext.evaluateString(
-      jsScope, 
-      formula, 
-      "DamageFormula: %s".format(formula),
-      1, 
-      null)
-    
-    println(jsResult)
-    
-    val resultAsInt = Context.toNumber(jsResult).round.toInt
-  
-    Context.exit()
-    
-    resultAsInt
+      resultAsInt
+    } catch {
+      case e: Throwable => {
+        logger.error(
+          "Error while calculating damage with formula: %s. ".format(formula) +
+  		  "Error: %s".format(e.getMessage()))
+  		-1
+      }
+    }
   }
 }
   
--- common/src/test/scala/rpgboss/model/DamageSpec.scala
+++ common/src/test/scala/rpgboss/model/DamageSpec.scala
@@ -16,17 +16,43 @@ class DamageSpec extends UnitSpec {
       Seq(),
       Seq(),
       0)
+
+    def testFormula(formula: String, expected: Int) = {
+      val d = Damage(formula = formula)
+      d.getBaseDamage(status, status) should equal(expected)
+    }
   }
 
-  "Damage" should "evaluate ATK and HP in the formula" in {
+  "Damage" should "evaluate single terms" in {
     val f = fixture
 
-    val status = f.status
-    status.stats should equal (
-      BattleStats(50, 20, 10, 10, 10, 10, 10, 
-                  f.pData.enums.elements.map(v => 0), Seq()))
+    f.testFormula("a.hp", 50)
+    f.testFormula("a.mp", 20)
+    f.testFormula("a.mhp", 50)
+    f.testFormula("a.mmp", 20)
+    f.testFormula("a.atk", 10)
+    f.testFormula("a.spd", 10)
+    f.testFormula("a.mag", 10)
+    f.testFormula("a.arm", 10)
+    f.testFormula("a.mre", 10)
+  }
+  
+  "Damage" should "do basic arithmetic" in {
+    val f = fixture
 
-    val damage = Damage(formula = "a.atk")
-    damage.getBaseDamage(status, status) should equal(10)
+    f.testFormula("a.hp + 10", 60)
+    f.testFormula("a.atk / 2", 5)
+  }
+  
+  "Damage" should "allow arithmetic between terms" in {
+    val f = fixture
+
+    f.testFormula("a.hp + 10 * a.atk", 150)
+  }
+  
+  "Damage" should "fail gracefully and return a sentinal value" in {
+    val f = fixture
+
+    f.testFormula("foo", -1)
   }
 }
