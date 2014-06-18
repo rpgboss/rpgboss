ee9c36324b6314a4acfc2cb95dd439c60b853bfc
Tommy C. Li <tommycli@keepword.com>
2014-06-17 18:29:25 -0700

    fix packaging for linux

--- package/linux/package.sh
+++ package/linux/package.sh
@@ -4,19 +4,21 @@ echo "======================"
 
 export SRC_DIR=$(cd "$(dirname "$0")"; pwd)
 
-rm -v $SRC_DIR/target/*
-mkdir $SRC_DIR/target
+rm -rfv $SRC_DIR/target/*
+export ARCHIVE_PATH=$SRC_DIR/target/rpgboss-$VERSION
+mkdir -p $ARCHIVE_PATH
 export JARPATH=$SRC_DIR/../../editor/target/scala-2.11/editor_2.11-0.1.min.jar
 
-cp -v $JARPATH $SRC_DIR/target/editor-min.jar
-cp -v $SRC_DIR/icon.png $SRC_DIR/rpgboss.desktop $SRC_DIR/target
-cp -v $SRC_DIR/launch.sh $SRC_DIR/target/rpgboss
+cp -v $JARPATH $ARCHIVE_PATH/editor-min.jar
+cp -v $SRC_DIR/icon.png $ARCHIVE_PATH
+cp -v $SRC_DIR/rpgboss.desktop $ARCHIVE_PATH
+cp -v $SRC_DIR/launch.sh $ARCHIVE_PATH/rpgboss
 
-chmod -v 755 $SRC_DIR/target/rpgboss
+chmod -v 755 $ARCHIVE_PATH/rpgboss
 
 export ARCHIVE="rpgboss-${VERSION}.tar.gz"
 
-tar -cvzf $SRC_DIR/target/$ARCHIVE -C $SRC_DIR/target .
+tar -cvzf $SRC_DIR/target/$ARCHIVE -C $SRC_DIR/target rpgboss-$VERSION
 
 cp -v $SRC_DIR/target/$ARCHIVE $SRC_DIR/../target/
 
