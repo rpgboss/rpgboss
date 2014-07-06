d9afe62ebbb72e5ffe1e9e2cfb8fed337f004fb2
Tommy C. Li <tommycli@keepword.com>
2014-07-05 19:34:27 -0700

    Fix 'Other Events' table editing not saving bug. Fixes #17

--- editor/src/main/scala/rpgboss/editor/uibase/TableEditor.scala
+++ editor/src/main/scala/rpgboss/editor/uibase/TableEditor.scala
@@ -9,34 +9,34 @@ import scala.collection.mutable.ArrayBuffer
 
 /**
  * A table that can be edited by context menu. Can add a new element by double
- * clicking the last empty row.
+ * clicking the last empty row. Edits modelArray "in-place" and calls
+ * onUpdate whenever its contents are updated.
  */
 abstract class TableEditor[T] extends ScrollPane {
   def title: String
-  
+
   def modelArray: ArrayBuffer[T]
   def newInstance(): T
-  
+
   /**
    * Called whenever modelArray changes.
    */
   def onUpdate()
-  
-  
+
   def colHeaders: Array[String]
   def getRowStrings(element: T): Array[String]
-  
+
   def showEditDialog(initial: T, okCallback: T => Unit)
-  
+
   border = BorderFactory.createTitledBorder(title)
-  
+
   def modelRowCount: Int = modelArray.length
-  
+
   val tableModel = new AbstractTableModel() {
     def getRowCount() = modelRowCount + 1 // last element blank for adding
     def getColumnCount() = colHeaders.length
     override def getColumnName(col: Int) = colHeaders(col)
-  
+
     def getValueAt(row: Int, col: Int) = {
       // There should be just one extra blank row at the end of the table.
       assume(row < modelRowCount + 1)
@@ -47,34 +47,36 @@ abstract class TableEditor[T] extends ScrollPane {
         "" // blank for new row
       }
     }
-  
+
     override def isCellEditable(r: Int, c: Int) = false
-  
+
   }
-  
+
   val table = new Table {
     model = tableModel
 
     selection.elementMode = Table.ElementMode.Row
     selection.intervalMode = Table.IntervalMode.Single
-    
+
     def editDialog(row: Int) = {
       val element = modelArray(row)
       showEditDialog(element, v => {
         modelArray.update(row, v)
         tableModel.fireTableRowsUpdated(row, row)
+        onUpdate()
       })
     }
-    
+
     def newDialog() = {
       val element = newInstance()
       showEditDialog(element, v => {
         modelArray += v
         tableModel.fireTableRowsUpdated(modelRowCount - 1, modelRowCount - 1)
         tableModel.fireTableRowsInserted(modelRowCount, modelRowCount)
+        onUpdate()
       })
     }
-    
+
     // TODO: Refactor the duplicate tableModel.fire ... calls
     listenTo(mouse.clicks)
     reactions += {
@@ -87,9 +89,9 @@ abstract class TableEditor[T] extends ScrollPane {
       }
       case e: MouseClicked if e.peer.getButton() == MouseEvent.BUTTON3 => {
         val (x0, y0) = (e.point.getX().toInt, e.point.getY().toInt)
-  
+
         val row = peer.rowAtPoint(e.point)
-  
+
         if (row != -1) {
           selection.rows.clear()
           selection.rows += row
@@ -97,7 +99,7 @@ abstract class TableEditor[T] extends ScrollPane {
             contents += new MenuItem(Action("New...") {
               newDialog()
             })
-  
+
             if (row != rowCount - 1) {
               contents += new MenuItem(Action("Edit...") {
                 editDialog(row)
@@ -105,6 +107,7 @@ abstract class TableEditor[T] extends ScrollPane {
               contents += new MenuItem(Action("Delete") {
                 modelArray.remove(row)
                 tableModel.fireTableRowsDeleted(row, row)
+                onUpdate()
               })
             }
           }
@@ -113,6 +116,6 @@ abstract class TableEditor[T] extends ScrollPane {
       }
     }
   }
-  
+
   contents = table
 }
