45cae2b6cb577c4b4dc167d7c13e275ee27cda6a
Tommy C. Li <tommycli@keepword.com>
2014-12-04 19:15:11 -0800

    Change internationalization to a singleton

--- editor/src/main/scala/rpgboss/editor/Internationalized.scala
+++ editor/src/main/scala/rpgboss/editor/Internationalized.scala
@@ -4,12 +4,12 @@ import java.util.Locale
 import java.util.ResourceBundle
 import com.typesafe.scalalogging.slf4j.LazyLogging
 
-trait Internationalized extends LazyLogging {
-  val messages =
-    ResourceBundle.getBundle("editorMessages", Internationalized.currentLocale)
-}
-
 object Internationalized extends LazyLogging {
-  val currentLocale = Locale.getDefault()
+  private val currentLocale = Locale.getDefault()
   logger.debug("Current locale: " + currentLocale.toString())
+
+  private val messages =
+    ResourceBundle.getBundle("editorMessages", Internationalized.currentLocale)
+
+  def getMessage(key: String) = messages.getString(key)
 }
