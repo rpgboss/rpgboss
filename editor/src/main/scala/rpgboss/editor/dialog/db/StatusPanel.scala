a53ab7f92f3d00b11c69a828aee3244b2f50751f
Caranille <hyppothalus@gmail.com>
2014-12-07 17:13:19 +0100

    Hot fix

--- editor/src/main/scala/rpgboss/editor/dialog/db/StatusPanel.scala
+++ editor/src/main/scala/rpgboss/editor/dialog/db/StatusPanel.scala
@@ -60,14 +60,14 @@ class StatusPanel(
           50,
           model.maxStacks = _)
 
-        row().grid(lbl(getMessage("Name" + ":"))).add(fName)
+        row().grid(lbl(getMessage("Name") + ":")).add(fName)
 
-        row().grid(lbl(getMessage("State_Expiration_In_Rounds" + ":"))).add(fReleaseTime)
-        row().grid(lbl(getMessage("Release_Chance_After_Expiry" + ":"))).add(fReleaseChance)
+        row().grid(lbl(getMessage("State_Expiration_In_Rounds") + ":")).add(fReleaseTime)
+        row().grid(lbl(getMessage("Release_Chance_After_Expiry") + ":")).add(fReleaseChance)
 
-        row().grid(lbl(getMessage("Release_After_Battle" + ":"))).add(fReleaseOnBattleEnd)
+        row().grid(lbl(getMessage("Release_After_Battle") + ":")).add(fReleaseOnBattleEnd)
 
-        row().grid(lbl(getMessage("Maximum_Stacks" + ":"))).add(fMaxStacks)
+        row().grid(lbl(getMessage("Maximum_Stacks") + ":")).add(fMaxStacks)
       }
 
       val rightPane =
