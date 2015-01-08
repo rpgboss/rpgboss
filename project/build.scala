6ef2efd33aa4b15ce00da24e1024b99353f58caa
Tommy C. Li <tommycli@keepword.com>
2015-01-07 18:35:28 -0800

    remove outdated chat dependencies

--- project/build.scala
+++ project/build.scala
@@ -22,9 +22,7 @@ object Settings {
       "net.sf.opencsv" % "opencsv" % "2.0" withSources(),
       "org.json4s" %% "json4s-native" % "3.2.10" withSources(),
       "org.scalatest" %% "scalatest" % "2.1.5" % "test",
-      "org.mozilla" % "rhino" % "1.7R4",
-      "javax.websocket" % "javax.websocket-api" % "1.1",
-      "org.glassfish.tyrus.bundles" % "tyrus-standalone-client" % "1.9"
+      "org.mozilla" % "rhino" % "1.7R4"
     ),
     unmanagedJars in Compile <<= baseDirectory map { base =>
       var baseDirectories = (base / "lib") +++ (base / "lib" / "extensions")
