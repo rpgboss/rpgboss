5f325e8d4f39679c281c7c3d2299fbb4248b03d5
Tommy C. Li <tommycli@keepword.com>
2015-02-05 22:57:22 -0800

    Fix item behavior in battles.

---
+++ common/src/test/scala/rpgboss/model/battle/BattleActionSpec.scala
@@ -0,0 +1,37 @@
+package rpgboss.model.battle
+
+import rpgboss._
+import rpgboss.model._
+import rpgboss.model.battle._
+import rpgboss.player.PersistentState
+
+class BattleActionSpec extends UnitSpec {
+  def fixture() = {
+    val f = new BattleTest.BattleFixture() {
+      val persistent = new PersistentState()
+      persistent.addRemoveItem(0, 10)
+      persistent.addRemoveItem(1, 10)
+      val fastCharStatus = battle.partyStatus.head
+    }
+    val dmg20 =
+    f.pData.enums.skills = Array(
+        Skill(effects = Array(Effect(RecoverHpAdd.id, 20))))
+    f.pData.enums.items = Array(
+        Item(effects = Array(Effect(RecoverHpAdd.id, 30))),
+        Item(useOnAttack = true, onUseSkillId = 0))
+    f
+  }
+
+  "ItemAction" should "work with direct item effects" in {
+    val f = fixture()
+
+    f.fastCharStatus.hp should equal(1)
+
+    val action =
+      ItemAction(f.fastCharStatus, Array(f.fastCharStatus), 0, f.persistent)
+    action.process(f.battle)
+
+    f.fastCharStatus.hp should equal(31)
+
+  }
+}
