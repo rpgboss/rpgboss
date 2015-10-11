894ba5f7d128b95a1633d5a55859e68c56625abf
Tommy C. Li <tommycli@keepword.com>
2015-10-11 11:35:52 -0700

    Partially move to gradle build.

---
+++ android/src/com/rpgboss/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.rpgboss.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.rpgboss.RpgBoss;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RpgBoss(), config);
+	}
+}
---
+++ desktop/src/com/rpgboss/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.rpgboss.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rpgboss.RpgBoss;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RpgBoss(), config);
+	}
+}
