a046fc31d1988f34fe27319303ded56a90741984
Jordan Irwin <antumdeluge@gmail.com>
2017-12-21 00:35:27 -0800

    Create update-enumerated script (CC0):
    Automatically fills "enumerated.txt".

---
+++ script/update-enumerated.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# This script is licensed under Creative Commons Zero (CC0).
+
+ROOT_DIR=$(dirname $(dirname $0))
+
+cd "${ROOT_DIR}/core/src/main/resources/defaultrc"
+
+IFS_orig="${IFS}"
+IFS=$'\n'
+
+rm -f "enumerated.txt"
+
+for FILE in $(ls); do
+  if [ -f "${FILE}" ]; then
+    echo "${FILE}" >> "enumerated.txt"
+  fi
+done
+
+for DIR in $(ls); do
+  if [ -d "${DIR}" ]; then
+    for FILE in $(find "${DIR}" -type f ! -name ".*"); do
+      echo "${FILE}" >> "enumerated.txt"
+    done
+  fi
+done
+
+IFS="${IFS_orig}"
