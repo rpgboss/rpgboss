d2c17d29a005e599a20fead0286d1bbdbbc6a76b
Jordan Irwin <antumdeluge@gmail.com>
2018-04-05 01:21:28 -0700

    Update Jsign URL in Windows packaging script

--- package/win/package.sh
+++ package/win/package.sh
@@ -3,16 +3,24 @@ echo "Packaging for Windows..."
 echo "========================"
 
 export SRC_DIR=$(cd "$(dirname "$0")"; pwd)
+export JSIGN_VER=2.0
+export JSIGN=jsign-$JSIGN_VER.jar
+export JSIGN_URL=https://github.com/ebourg/jsign/releases/download/$JSIGN_VER/$JSIGN
 
 rm -rfv $SRC_DIR/target
 mkdir $SRC_DIR/target/
 
-launch4j $SRC_DIR/config-win.xml 
+launch4j $SRC_DIR/config-win.xml
 launch4j $SRC_DIR/config-win-player.xml
 
-#cd $SRC_DIR/target
-#wget http://people.apache.org/~ebourg/jsign/jsign-1.2.jar
-#java -jar jsign-1.2.jar --keystore $SRC_DIR/../../keystore.jks --storepass password --alias selfsigned --name "rpgboss" --url rpgboss.com rpgboss-editor.exe
+cd $SRC_DIR/target
+
+if [ -f "$SRC_DIR/../../keystore.jks" ]; then
+	wget $JSIGN_URL
+	java -jar $JSIGN --keystore $SRC_DIR/../../keystore.jks --storepass password --alias selfsigned --name "rpgboss" --url rpgboss.com rpgboss-editor.exe
+else
+	echo "WARNING: keystore.jks not found, executable will not be signed"
+fi
 
 cp -v $SRC_DIR/target/rpgboss-editor.exe $SRC_DIR/../target/rpgboss-${VERSION}.exe
 echo ""
