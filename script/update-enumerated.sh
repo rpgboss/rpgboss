9efa5ba24b7d59dbaa644ea8eddd31bf580a4c90
Jordan Irwin <antumdeluge@gmail.com>
2018-04-29 18:32:55 -0700

    Fix update-enumerated script

--- script/update-enumerated.sh
+++ script/update-enumerated.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script is licensed under Creative Commons Zero (CC0).
 
@@ -6,23 +6,22 @@ ROOT_DIR=$(dirname $(dirname $0))
 
 cd "${ROOT_DIR}/core/src/main/resources/defaultrc"
 
-IFS_orig="${IFS}"
-IFS=$'\n'
-
 rm -f "enumerated.txt"
 
-for FILE in $(ls); do
+echo -e "\nDir:\t."
+for FILE in *; do
   if [ -f "${FILE}" ]; then
+    echo -e "Adding file:\t${FILE}"
     echo "${FILE}" >> "enumerated.txt"
   fi
 done
 
-for DIR in $(ls); do
+for DIR in *; do
   if [ -d "${DIR}" ]; then
+    echo -e "\nDir:\t${DIR}"
     for FILE in $(find "${DIR}" -type f ! -name ".*" ! -name "*.xcf" ! -name "*.psd" ! -name "*.svg"); do
+      echo -e "Adding file:\t${FILE}"
       echo "${FILE}" >> "enumerated.txt"
     done
   fi
 done
-
-IFS="${IFS_orig}"
